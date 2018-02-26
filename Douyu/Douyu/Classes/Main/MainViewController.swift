5fdbe602deb6f003d59d5100082a4e739034f971
zhangjili <lakshya@sohu.com>
2018-02-26 17:09:39 +0800

    h

--- Douyu/Douyu/Classes/Main/MainViewController.swift
+++ Douyu/Douyu/Classes/Main/MainViewController.swift
@@ -8,28 +8,15 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let childVC=UIStoryboard(name:"Home",bundle:nil).instantiateInitialViewController()!
         // Do any additional setup after loading the view.
+        addChildViewController(childVC)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
